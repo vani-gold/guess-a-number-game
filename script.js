cd95eec8ee35d4c8b94e02d865c56c8f549d9bd2
vani-gold <esibevan@yahoo.com>
2022-10-13 13:28:13 +0100

    implement final code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     let range = 2;
     let stage = 1;
-    let Winner = 0;
+    let counter = 0;
 
   // prompt and condition function
   function getUsersPrompt() {
@@ -19,39 +19,51 @@
     if (!cNum){
         document.querySelector('.uchoose').textContent = 'No Number';
     }else if(cNum === userPrompt){
-
+        counter++;
         range++;
         stage++;
         console.log(`The Random Num is ${cNum}`);
         console.log(`The New range is 1 to ${range}`);
         console.log(`The User prompt is ${userPrompt}`);
+        document.querySelector('.winner').textContent = counter;
         document.querySelector('.cchoose').textContent = cNum;
         document.querySelector('.stage').textContent = stage;
         document.querySelector('.range').textContent = range;
         document.querySelector('.match').textContent = '🎉 A MATCH';
-        document.querySelector('.match').style.color = '#f542d7';
+        document.querySelector('body').style.backgroundColor = '#f542d7';
+        // document.querySelector('body').style.color = '#f542d7';
+        
       } else if(cNum >userPrompt ) {
+        counter++
         console.log(`The Random Num is ${cNum}`);
         console.log(`The range is 1 to ${range}`);
         console.log(`The User prompt is ${userPrompt}`);
+        document.querySelector('.winner').textContent = counter;
         document.querySelector('.cchoose').textContent = cNum;
         document.querySelector('.uchoose').textContent = `${userPrompt}`;
         document.querySelector('.cchoose').textContent = `${cNum}`;
         document.querySelector('.match').textContent = '👿 NOT A MATCH';
+        document.querySelector('body').style.backgroundColor = '#000000';
+        // document.querySelector('body').style.backgroundColor = '#FFFFFF';
+
      } else if(cNum<userPrompt){
+      counter++
       console.log(`The Random Num is ${cNum}`);
       console.log(`The range is 1 to ${range}`);
       console.log(`The User prompt is ${userPrompt}`);
       document.querySelector('.cchoose').textContent = cNum;
+      document.querySelector('.winner').textContent = counter;
       document.querySelector('.uchoose').textContent = `${userPrompt}`;
       document.querySelector('.cchoose').textContent = `${cNum}`;
       document.querySelector('.match').textContent = '👿 NOT A MATCH';
-     }
+      document.querySelector('body').style.backgroundColor = '	#000000';
+      // document.querySelector('body').style.backgroundColor = '#FFFFFF';
     }
+  }
 
+    // Listen for a click event
   let btn = document.querySelector('button');
     btn.addEventListener("click", getUsersPrompt);
-  
 
   
 
