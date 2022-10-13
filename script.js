9bb0c6679d2e6ec32b8ff47d3714e69fc2e1fcf9
vani-gold <esibevan@yahoo.com>
2022-10-13 08:48:00 +0100

    implemented the console output

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
     let range = 2;
     let stage = 1;
+    let Winner = 0;
 
-    // document.querySelector('.btn check').addEventListener('click', function(range){
-    //   let cNum = Math.floor(Math.random() * range) + 1;
-    //   console.log(cNum);
-    //   } );
-    //   const getComputerValue= function(range){
-    //   return Math.floor(Math.random() * range) + 1;
-    // }
-    // const cNum = getComputerValue(range);
-    // document.querySelector('.cchoose').textContent = cNum;
-
-    // console.log(num)
-    // const cNum = Math.trunc(Math.random()* 10)+1;
-    // document. querySelector('.cchoose').textContent = cNum;
-    // let range = 2;
-
-   
-    
   // prompt and condition function
   function getUsersPrompt() {
     const userPrompt = Number(prompt('Guess a Number based on current Range'));
@@ -35,31 +19,39 @@
     if (!cNum){
         document.querySelector('.uchoose').textContent = 'No Number';
     }else if(cNum === userPrompt){
+
         range++;
         stage++;
-        console.log(range);
+        console.log(`The Random Num is ${cNum}`);
+        console.log(`The New range is 1 to ${range}`);
+        console.log(`The User prompt is ${userPrompt}`);
         document.querySelector('.cchoose').textContent = cNum;
         document.querySelector('.stage').textContent = stage;
         document.querySelector('.range').textContent = range;
         document.querySelector('.match').textContent = '🎉 A MATCH';
-        document.querySelector('body').style.backgroundColor = '#f542d7';
+        document.querySelector('.match').style.color = '#f542d7';
       } else if(cNum >userPrompt ) {
+        console.log(`The Random Num is ${cNum}`);
+        console.log(`The range is 1 to ${range}`);
+        console.log(`The User prompt is ${userPrompt}`);
         document.querySelector('.cchoose').textContent = cNum;
         document.querySelector('.uchoose').textContent = `${userPrompt}`;
         document.querySelector('.cchoose').textContent = `${cNum}`;
         document.querySelector('.match').textContent = '👿 NOT A MATCH';
-
+     } else if(cNum<userPrompt){
+      console.log(`The Random Num is ${cNum}`);
+      console.log(`The range is 1 to ${range}`);
+      console.log(`The User prompt is ${userPrompt}`);
+      document.querySelector('.cchoose').textContent = cNum;
+      document.querySelector('.uchoose').textContent = `${userPrompt}`;
+      document.querySelector('.cchoose').textContent = `${cNum}`;
+      document.querySelector('.match').textContent = '👿 NOT A MATCH';
      }
     }
 
   let btn = document.querySelector('button');
     btn.addEventListener("click", getUsersPrompt);
-    // let b = document.querySelector('button');
-    // b.addEventListener("click", getComputerValue);
- 
-    // let cNum = result(range); 
-    // const cNum = getComputerValue(range);
-    // document.querySelector('.cchoose').textContent = cNum;
+  
 
   
 
