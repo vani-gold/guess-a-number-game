b4f41ad05f0c8ea1dad0fae6ece096ecd352a0c5
vani-gold <esibevan@yahoo.com>
2022-10-13 15:19:02 +0100

    implement final code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # guess-a-number-game
-Create a number guessing game to generate a number between the range of 1 and 2...
+Task Title: Week 4 NodeJS Task: Inheritance, Functions / Arrow Functions, Cookies, Promises and Async
+
+replit link: https://computerVsHumanGame.esibevanessa.repl.co
--- script.js
+++ script.js
@@ -2,8 +2,13 @@
     let stage = 1;
     let counter = 0;
 
+    const userName = (prompt('GIVE YOUR USER NAME'));
+    console.log(userName);
+      //  document.querySelector('.label').textContent = `${userName}Choose:`;
+      //  document.querySelector('.stage').textContent = stage;
   // prompt and condition function
   function getUsersPrompt() {
+   
     const userPrompt = Number(prompt('Guess a Number based on current Range'));
 // second function
     const getComputerValue= function(range){
@@ -11,7 +16,7 @@
     }
     const cNum = getComputerValue(range);
     document.querySelector('.cchoose').textContent = cNum;
-
+    // document.querySelector('.label').textContent = `${userName}Choose:`;
     // let cNum = num;
     document.querySelector('.uchoose').textContent = userPrompt;
     // document.querySelector('.cchoose').textContent = cNum;
@@ -24,11 +29,12 @@
         stage++;
         console.log(`The Random Num is ${cNum}`);
         console.log(`The New range is 1 to ${range}`);
-        console.log(`The User prompt is ${userPrompt}`);
+        console.log(`${userName} prompt is ${userPrompt}`);
         document.querySelector('.winner').textContent = counter;
         document.querySelector('.cchoose').textContent = cNum;
         document.querySelector('.stage').textContent = stage;
         document.querySelector('.range').textContent = range;
+        document.querySelector('.uchoose').textContent = `${userPrompt}`;
         document.querySelector('.match').textContent = '🎉 A MATCH';
         document.querySelector('body').style.backgroundColor = '#f542d7';
         // document.querySelector('body').style.color = '#f542d7';
@@ -40,6 +46,7 @@
         console.log(`The User prompt is ${userPrompt}`);
         document.querySelector('.winner').textContent = counter;
         document.querySelector('.cchoose').textContent = cNum;
+        // document.querySelector('label').textContent = `${userName} Choose`;
         document.querySelector('.uchoose').textContent = `${userPrompt}`;
         document.querySelector('.cchoose').textContent = `${cNum}`;
         document.querySelector('.match').textContent = '👿 NOT A MATCH';
@@ -53,10 +60,11 @@
       console.log(`The User prompt is ${userPrompt}`);
       document.querySelector('.cchoose').textContent = cNum;
       document.querySelector('.winner').textContent = counter;
+      // document.querySelector('label').textContent = `${userName} Choose`;
       document.querySelector('.uchoose').textContent = `${userPrompt}`;
       document.querySelector('.cchoose').textContent = `${cNum}`;
       document.querySelector('.match').textContent = '👿 NOT A MATCH';
-      document.querySelector('body').style.backgroundColor = '	#000000';
+      document.querySelector('body').style.backgroundColor = '#000000';
       // document.querySelector('body').style.backgroundColor = '#FFFFFF';
     }
   }
